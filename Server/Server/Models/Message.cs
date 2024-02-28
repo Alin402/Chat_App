1019a92e4ef9ba4b10c1291868b08bcb090f5b56
Balanescu Alin <balanescu.alin03@gmail.com>
2024-02-28 20:48:22 +0200

    finished initial rough setup of the server

---
+++ Server/Server/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class Message
+    {
+        public User From { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
+    }
+}
