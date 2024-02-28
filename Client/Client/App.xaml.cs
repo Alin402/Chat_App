d0bc8248a0258c6c1456e9b560cc5af2e371a781
Balanescu Alin <balanescu.alin03@gmail.com>
2024-02-28 17:18:00 +0200

    in preogress

--- Client/Client/App.xaml.cs
+++ Client/Client/App.xaml.cs
@@ -9,6 +9,11 @@ namespace Client
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            UserNameWindow wnd = new UserNameWindow();
+            wnd.Show();
+        }
     }
 
 }
---
+++ Client/Client/utils/WindowUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client.utils
+{
+    public class WindowUtils
+    {
+        public static void OpenNewWindowAndCloseCurrentOne(Window newWindow, Window currentWindow)
+        {
+            newWindow.Show();
+            currentWindow.Close();
+        }
+    }
+}
